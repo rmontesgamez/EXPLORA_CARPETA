401649aafb909cc84e66a963e075293705c7a699
rmontesgamez <rmontesgamez@hotmail.com>
2022-09-20 11:37:22 +0200

    inicio

--- EXPLORA_CARPETA/EXPLORA_CARPETA.py
+++ EXPLORA_CARPETA/EXPLORA_CARPETA.py
@@ -1,43 +1,67 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import os
 
-class filedialogdemo(QWidget):
+from PyQt5.QtWidgets import QWidget, QFileDialog, QApplication, QVBoxLayout, QPushButton
+
+class filedialog(QWidget):
     def __init__(self):
-        super(filedialogdemo,self).__init__()
+        super(filedialog,self).__init__()
 
         layout = QVBoxLayout()
         self.btn = QPushButton("SELECCIONA ARCHIVO EN CARPETA")
         self.btn.clicked.connect(self.getfile)
 
         layout.addWidget(self.btn)
-        """
-        self.le = QLabel("Hello")
-
-        layout.addWidget(self.le)
-        self.btn1 = QPushButton("QFileDialog object")
-        self.btn1.clicked.connect(self.getfiles)
-        layout.addWidget(self.btn1)
-
-        self.contents = QTextEdit()
-        layout.addWidget(self.contents)
-        """
+        
         self.setLayout(layout)
         self.setWindowTitle("File Dialog demo")
 
 
     def getfile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file',
-                                            'c:\\activa\\',"Image files (*.dxf *.dwg)")
+        """
+        función para abrir desplegable para elegir una carpeta
+        entrada
+        clase
+
+        salida
+        string con la ruta de la carpeta
+
+        """
+
+        fname = QFileDialog.getExistingDirectory(self, 'Open file',
+                                            'c:\\activa\\')
 
         print(fname)
+
+        lista_archivos(fname)
         #self.le.setPixmap(QPixmap(fname))
 
+def lista_archivos(ruta):
+    """
+    funcion para obtener la lista de archivos de una carpeta
+
+    entrada
+    string  Ruta de la carpeta
+
+    salida
+    lista de string con nombres de archivos limitados a 50 caracteres
+    """
+    contenido = os.scandir(ruta)
+    listado=[]
+    for elemento in contenido:
+        if elemento.is_file() and elemento.name.endswith(".dxf"):
+            listado.append(elemento.name[:49].split(".")[0])
+            
+    print(listado)
 
 def main():
+    """
+    función main
+
+    """
+
     app = QApplication(sys.argv)
-    ex = filedialogdemo()
+    ex = filedialog()
     ex.show()
     sys.exit(app.exec_())
 
